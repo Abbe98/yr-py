406dfd805a7e17f727e905103f33b3a74ca29d67
Albin Larsson <albin.post@gmail.com>
2016-10-01 16:13:09 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
   name = 'yr',
   packages = ['yr'], # this must be the same as the name above
   install_requires=['lxml'],
-  version = '0.4',
+  version = '0.5',
   description = 'Wrapper for the YR.no API, for near time weather prediction.',
   author = 'Albin Larsson',
   author_email = 'albin.post@gmail.com',
   url = 'https://github.com/Abbe98/yr-py', # use the URL to the github repo
-  download_url = 'https://github.com/Abbe98/yr-py/tarball/0.4', # I'll explain this in a second
+  download_url = 'https://github.com/Abbe98/yr-py/tarball/0.5', # I'll explain this in a second
   keywords = ['weather', 'yr', 'yr.no'], # arbitrary keywords
   classifiers = [],
 )
